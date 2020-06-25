09cd251949500ec15ffbeb6ff4737276a384f90c
Jauregui1 <isaacjaureguih@gmail.com>
2020-06-25 17:03:11 -0500

    Initial Commit

---
+++ pizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pizza
+//
+//  Created by Isaac Jauregui Hernandez on 25/06/20.
+//  Copyright © 2020 Isaac Jauregui Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
